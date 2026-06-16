9cb1a9941addd7572a78961fcb7a8abe12e9c9d2
BraisedPork <46232992+braisedpork1964@users.noreply.github.com>
2026-06-16 16:16:54 +0800

    [Feat] Add OpenHandsAdapter (#349)
    add OpenHandsAdapter

--- lagent/adapters/__init__.py
+++ lagent/adapters/__init__.py
@@ -4,6 +4,7 @@ from .claude_code_sdk import ClaudeCodeSDKAdapter
 from .cli_adapter import CLIAgentAdapter
 from .opencode_cli import OpenCodeCLIAdapter
 from .mini_swe_agent import MiniSWEAgentAdapter
+from .openhands import OpenHandsAdapter
 from .proxy import SessionClient
 from .sdk_adapter import SDKAgentAdapter
 
@@ -14,6 +15,7 @@ __all__ = [
     'OpenCodeCLIAdapter',
     'ClaudeCodeAdapter',
     'ClaudeCodeSDKAdapter',
+    'OpenHandsAdapter',
     'SDKAgentAdapter',
     'MiniSWEAgentAdapter',
     'SessionClient',
